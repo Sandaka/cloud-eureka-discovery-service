c9763c8c67435086903e95dd9819bef1998c5925
Sandaka <sandaka94@gmail.com>
2021-10-17 23:33:57 +0530

    first commit

---
+++ src/main/java/com/kingston/msc/CloudEurekaDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kingston.msc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingston/msc/CloudEurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingston.msc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
