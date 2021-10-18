a4e2c9b562e7f1f54e174b827a394193c8c9ce38
Sandaka <sandaka94@gmail.com>
2021-10-19 01:05:30 +0530

    Microservices initial implementation

--- src/main/java/com/kingston/msc/CloudEurekaDiscoveryServiceApplication.java
+++ src/main/java/com/kingston/msc/CloudEurekaDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.kingston.msc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudEurekaDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
